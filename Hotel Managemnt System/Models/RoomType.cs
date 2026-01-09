ccc85d4eddfa0a5905c3d20f907a0570d21657d0
Julliebert <cogueronjulliebert21@gmail.com>
2026-01-09 20:51:23 +0800

    Booking Page

---
+++ Hotel Managemnt System/Models/RoomType.cs
@@ -0,0 +1,9 @@
+﻿namespace Hotel_Managemnt_System.Models
+{
+    public class RoomType
+    {
+        public Guid RoomTypeId { get; set; }
+        public string RoomTypeName { get; set; }
+        public decimal RoomTypePrice { get; set; }
+    }
+}
---
+++ Hotel Managemnt System/Pages/Bookingpage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hotel_Managemnt_System.Pages
+{
+    public class BookingpageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
